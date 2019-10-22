dd08dd72e72798b7e90542f96f4c5f9b386add7d
taishicheng-ds <123.gome>
2019-10-22 16:48:08 +0800

    整合turbine

---
+++ demo-turbine/src/test/java/com/example/demoturbine/DemoTurbineApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demoturbine;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
