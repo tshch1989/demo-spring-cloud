537b9299a2cb1caef920bf9298315806cf66c44c
tsc <tshch1989@sina.com>
2019-12-30 19:48:12 +0800

    工程命名做了一些修整

--- demo-zuul/src/main/java/com/example/demozuul/MetaDataZuulFilter.java
+++ demo-zuul/src/main/java/com/example/demozuul/MetaDataZuulFilter.java
@@ -1,6 +1,7 @@
 package com.example.demozuul;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import io.jmnarloch.spring.cloud.ribbon.support.RibbonFilterContextHolder;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
@@ -25,8 +26,13 @@ public class MetaDataZuulFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        String hostMark = context.getRequest().getHeader("host-mark");
+        if(hostMark == null){
+            hostMark = "gray-mark";
+        }
         RibbonFilterContextHolder.getCurrentContext()
-                .add("host-mark","gray-mark");
+                .add("host-mark",hostMark);
         return null;
     }
 }
